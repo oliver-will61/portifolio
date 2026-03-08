35adf9cd2c146182c1e138105f8904e711ac881b
Willian <will.oliveira.2408@gmail.com>
2026-03-08 15:18:12 -0300

    removido os .. do caminho do arquivo

--- constants/habilidades.js
+++ constants/habilidades.js
@@ -3,192 +3,192 @@ export const listaHabilidades = [
     {
         titulo: "TypeScript",
         icones: [{
-            iconeDark: "../assets/icones-habilidades/typescript.svg",
-            iconeLight: "../assets/icones-habilidades/typescript.svg"
+            iconeDark: "assets/icones-habilidades/typescript.svg",
+            iconeLight: "assets/icones-habilidades/typescript.svg"
         }]
     },
 
     {
         titulo: "Python",
         icones: [{
-            iconeDark: "../assets/icones-habilidades/python.svg",
-            iconeLight: "../assets/icones-habilidades/python.svg"
+            iconeDark: "assets/icones-habilidades/python.svg",
+            iconeLight: "assets/icones-habilidades/python.svg"
         }]
     },
 
     {
         titulo: "JavaScript",
         icones: [{
-            iconeDark: "../assets/icones-habilidades/js.svg",
-            iconeLight: "../assets/icones-habilidades/js.svg"
+            iconeDark: "assets/icones-habilidades/js.svg",
+            iconeLight: "assets/icones-habilidades/js.svg"
         }]
     },
 
     {
         titulo: "Node.JS",
         icones: [{
-            iconeDark: "../assets/icones-habilidades/nodejs.svg",
-            iconeLight: "../assets/icones-habilidades/nodejs.svg"
+            iconeDark: "assets/icones-habilidades/nodejs.svg",
+            iconeLight: "assets/icones-habilidades/nodejs.svg"
         }]
     },
 
     {
         titulo: "MySQL",
         icones: [{
-            iconeDark: "../assets/icones-habilidades/mysql.svg",
-            iconeLight: "../assets/icones-habilidades/mysql.svg"
+            iconeDark: "assets/icones-habilidades/mysql.svg",
+            iconeLight: "assets/icones-habilidades/mysql.svg"
         }]
     },
 
     {
         titulo: "Git",
         icones: [{
-            iconeDark: "../assets/icones-habilidades/git.svg",
-            iconeLight: "../assets/icones-habilidades/git.svg"
+            iconeDark: "assets/icones-habilidades/git.svg",
+            iconeLight: "assets/icones-habilidades/git.svg"
         }]
     },
 
     {
         titulo: "GitHub",
         icones: [{
-            iconeDark: "../assets/icones-habilidades/github.svg",
-            iconeLight: "../assets/icones-habilidades/github-light.svg"
+            iconeDark: "assets/icones-habilidades/github.svg",
+            iconeLight: "assets/icones-habilidades/github-light.svg"
         }]
     },
 
     {
         titulo: "Postman",
         icones: [{
-            iconeDark: "../assets/icones-habilidades/postman.svg",
-            iconeLight: "../assets/icones-habilidades/postman.svg"
+            iconeDark: "assets/icones-habilidades/postman.svg",
+            iconeLight: "assets/icones-habilidades/postman.svg"
         }]
     },
 
     {
         titulo: "Linux",
         icones: [{
-            iconeDark: "../assets/icones-habilidades/linux.svg",
-            iconeLight: "../assets/icones-habilidades/linux.svg"
+            iconeDark: "assets/icones-habilidades/linux.svg",
+            iconeLight: "assets/icones-habilidades/linux.svg"
         }]
     },
 
     {
         titulo: "Java",
         icones: [{
-            iconeDark: "../assets/icones-habilidades/java.svg",
-            iconeLight: "../assets/icones-habilidades/java.svg"
+            iconeDark: "assets/icones-habilidades/java.svg",
+            iconeLight: "assets/icones-habilidades/java.svg"
         }]
     },
 
     {
         titulo: "Spring Boot",
         icones: [{
-            iconeDark: "../assets/icones-habilidades/spring.svg",
-            iconeLight: "../assets/icones-habilidades/spring.svg"
+            iconeDark: "assets/icones-habilidades/spring.svg",
+            iconeLight: "assets/icones-habilidades/spring.svg"
         }]
     },
 
     {
         titulo: "HTML",
         icones: [{
-            iconeDark: "../assets/icones-habilidades/html5.svg",
-            iconeLight: "../assets/icones-habilidades/html5.svg"
+            iconeDark: "assets/icones-habilidades/html5.svg",
+            iconeLight: "assets/icones-habilidades/html5.svg"
         }]
     },
 
     {
         titulo: "CSS3",
         icones: [{
-            iconeDark: "../assets/icones-habilidades/css3.svg",
-            iconeLight: "../assets/icones-habilidades/css3.svg"
+            iconeDark: "assets/icones-habilidades/css3.svg",
+            iconeLight: "assets/icones-habilidades/css3.svg"
         }]
     },
 
     {
         titulo: "AWS",
         icones: [{
-            iconeDark: "../assets/icones-habilidades/aws.svg",
-            iconeLight: "../assets/icones-habilidades/aws-light.svg"
+            iconeDark: "assets/icones-habilidades/aws.svg",
+            iconeLight: "assets/icones-habilidades/aws-light.svg"
         }]
     },
 
     {
         titulo: "Express",
         icones: [{
-            iconeDark: "../assets/icones-habilidades/express.svg",
-            iconeLight: "../assets/icones-habilidades/express-light.svg"
+            iconeDark: "assets/icones-habilidades/express.svg",
+            iconeLight: "assets/icones-habilidades/express-light.svg"
         }]
     },
 
     {
         titulo: "Bootstrap",
         icones: [{
-            iconeDark: "../assets/icones-habilidades/bootstrap.svg",
-            iconeLight: "../assets/icones-habilidades/bootstrap.svg"
+            iconeDark: "assets/icones-habilidades/bootstrap.svg",
+            iconeLight: "assets/icones-habilidades/bootstrap.svg"
         }]
     },
 
     {
         titulo: "Docker",
         icones: [{
-            iconeDark: "../assets/icones-habilidades/docker.svg",
-            iconeLight: "../assets/icones-habilidades/docker.svg"
+            iconeDark: "assets/icones-habilidades/docker.svg",
+            iconeLight: "assets/icones-habilidades/docker.svg"
         }]
     },
 
     {
         titulo: "Flutter",
         icones: [{
-            iconeDark: "../assets/icones-habilidades/flutter.svg",
-            iconeLight: "../assets/icones-habilidades/flutter.svg"
+            iconeDark: "assets/icones-habilidades/flutter.svg",
+            iconeLight: "assets/icones-habilidades/flutter.svg"
         }]
     },
 
     {
         titulo: "MongoDB",
         icones: [{
-            iconeDark: "../assets/icones-habilidades/mongodb.svg",
-            iconeLight: "../assets/icones-habilidades/mongodb.svg"
+            iconeDark: "assets/icones-habilidades/mongodb.svg",
+            iconeLight: "assets/icones-habilidades/mongodb.svg"
         }]
     },
 
     {
         titulo: "GraphQL",
         icones: [{
-            iconeDark: "../assets/icones-habilidades/graphql.svg",
-            iconeLight: "../assets/icones-habilidades/graphql.svg"
+            iconeDark: "assets/icones-habilidades/graphql.svg",
+            iconeLight: "assets/icones-habilidades/graphql.svg"
         }]
     },
 
     {
         titulo: "Jira",
         icones: [{
-            iconeDark: "../assets/icones-habilidades/jira.svg",
-            iconeLight: "../assets/icones-habilidades/jira.svg"
+            iconeDark: "assets/icones-habilidades/jira.svg",
+            iconeLight: "assets/icones-habilidades/jira.svg"
         }]
     },
 
     {
         titulo: "Figma",
         icones: [{
-            iconeDark: "../assets/icones-habilidades/figma.svg",
-            iconeLight: "../assets/icones-habilidades/figma.svg"
+            iconeDark: "assets/icones-habilidades/figma.svg",
+            iconeLight: "assets/icones-habilidades/figma.svg"
         }]
     },
 
     {
         titulo: "Pandas",
         icones: [{
-            iconeDark: "../assets/icones-habilidades/pandas.svg",
-            iconeLight: "../assets/icones-habilidades/pandas-light.svg"
+            iconeDark: "assets/icones-habilidades/pandas.svg",
+            iconeLight: "assets/icones-habilidades/pandas-light.svg"
         }]
     },
 
     {
         titulo: "Pygame",
         icones: [{
-            iconeDark: "../assets/icones-habilidades/pygame.svg",
-            iconeLight: "../assets/icones-habilidades/pygame.svg"
+            iconeDark: "assets/icones-habilidades/pygame.svg",
+            iconeLight: "assets/icones-habilidades/pygame.svg"
         }]
     }
 ]
