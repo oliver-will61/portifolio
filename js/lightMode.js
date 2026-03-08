e8d057b70bc95807ca8614ffb3f705cbe46673c8
Willian <will.oliveira.2408@gmail.com>
2026-03-08 14:58:54 -0300

    ajustes no css

--- js/lightMode.js
+++ js/lightMode.js
@@ -6,8 +6,18 @@ function lightMode(){
     const btnSwitch = document.querySelector("#switch-input") 
     const body = document.querySelector('body')
 
+    // Variável para armazenar a posição do scroll
+    let scrollPosition = 0
+
+    function toggleTheme() {
+        
+    }
+    
 
     btnSwitch.addEventListener('change', function () {
+
+        // Salva a posição atual do scroll antes da mundaça do modo claro ou escuro
+        scrollPosition = window.scrollY
 
         if (this.checked) {
             // this se refere ao btnSwitch( que é um input do tipo checkbox), portanto 
@@ -19,6 +29,11 @@ function lightMode(){
             carregaHabilidades('light')
             carregaCardProjetos('light')
 
+             //restaura o scroll para a posição anterior da mudança, evitando o deslocamento indevido para o topo da página
+            requestAnimationFrame(() => {
+                window.scrollTo(0, scrollPosition)
+            })
+
 
         } else {
             body.classList.remove("ligth-mode")
@@ -26,6 +41,11 @@ function lightMode(){
             //carrega os icones padrões (dark mode)
             carregaHabilidades()
             carregaCardProjetos()
+            
+            //restaura o scroll para a posição anterior da mudança, evitando o deslocamento indevido para o topo da página
+            requestAnimationFrame(() => {
+                window.scrollTo(0, scrollPosition)
+            })
         }
         
     })    
